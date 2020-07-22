d95eb1f3fe9a46a57f91e70ea1e6bd8c06faa378
Dani Coleman <danicoleman00@gmail.com>
2020-07-22 13:01:54 -0600

    Create library class

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_reader :first_name,
+              :last_name,
+              :name,
+              :books
+  def initialize(info)
+    @first_name = info[:first_name]
+    @last_name  = info[:last_name]
+    @name       = info[:first_name] + " " + info[:last_name]
+    @books      = []
+  end
+
+  def write(title, publication_year)
+    new_books = Book.new({
+      title: title,
+      publication_date: publication_year,
+      author_first_name: first_name,
+      author_last_name: last_name
+    })
+
+    @books << new_books
+    return new_books
+  end
+end
---
+++ lib/library.rb
@@ -0,0 +1,20 @@
+class Library
+  attr_reader :name,
+              :books,
+              :authors
+
+  def initialize(name)
+    @name    = name
+    @books   = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
+    @books += author.books
+  end
+
+  def publication_time_frame_for(author)
+    
+  end
+end
