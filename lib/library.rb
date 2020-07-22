75ce4c9c4c7274aada290face3b7bb0c16cc543e
Dani Coleman <danicoleman00@gmail.com>
2020-07-22 14:25:02 -0600

    Add checkout methodd

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,14 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
 
   def initialize(name)
-    @name    = name
-    @books   = []
-    @authors = []
+    @name              = name
+    @books             = []
+    @authors           = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -20,5 +22,14 @@ class Library
       start: books_by_date.first.publication_year,
       end: books_by_date.last.publication_year
     }
+  end
+
+  def checkout(book)
+    if @books.include?(book)
+      @checked_out_books << book
+      true
+    else
+      false
+    end
   end
 end
